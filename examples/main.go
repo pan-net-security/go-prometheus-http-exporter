0216cacd90b663ca9a82ac12a930ed6f8bc2f1c8
Matúš Ferech <matus.ferech@pan-net.eu>
2022-03-16 14:29:12 +0100

    Initial commit

---
+++ examples/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	prom_http_exporter "github.com/pan-net-security/go-prometheus-http-exporter"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(200 * time.Millisecond)
+	w.WriteHeader(301)
+	w.Write([]byte("foo"))
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(401)
+	w.Write([]byte("bar"))
+}
+
+func main() {
+	e := prom_http_exporter.New()
+
+	r := http.NewServeMux()
+	r.Handle(e.Metric("/foo", foo))
+	r.Handle(e.Metric("/bar", bar))
+	r.Handle("/metrics", promhttp.Handler())
+
+	s := &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		Handler:      r,
+	}
+	log.Fatalln(http.ListenAndServe(":8080", s.Handler))
+}
